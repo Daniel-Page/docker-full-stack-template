c1c926a9fdd1bf9442411d05b02673032104696c
Daniel Page <dwpage98@gmail.com>
2022-07-08 11:08:27 +1200

    Users printed line by line 3

--- src/public/coms.js
+++ src/public/coms.js
@@ -5,9 +5,13 @@ const email = document.getElementById("email");
 const getUsers = async () => {
   const response = await fetch("/get-users");
   const myJson = await response.json();
-  for (user in myJson.Users) {
-    textBox.innerText += myJson.Users[user];
-    textBox.innerText += "\n";
+  for (let i=0;i<myJson.Users.length;i++) {
+    textBox.innerText += myJson.Users[i].name;
+    textBox.innerText += " ";
+    textBox.innerText += myJson.Users[i].email;
+    if (i!=myJson.Users.length-1) {
+      textBox.innerText += "\n";
+    }
   }
 };
 
